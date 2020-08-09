0d7abbeb14b545c212924362c9b285af7264a207
Peter Barraud <barraud@adobe.com>
2020-08-09 19:06:24 +0530

    get started

--- getting-started.py
+++ getting-started.py
@@ -1,5 +1,14 @@
+def addnumbers(i, j):
+    if i.isnumeric() and j.isnumeric():
+        print(int(i) + int(j))
+    else:
+        print("something isn't right")
+
 def main():
-    print(0)
+    # take user input
+    i = input('enter a number: ')
+    j = input('enter another number: ')
+    addnumbers(i, j)
 
 if __name__ == "__main__":
     main()
