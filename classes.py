f14c21ee131e9c35a76edc1a416d1e73c2dccfbb
Peter Barraud <barraud@adobe.com>
2020-08-09 20:03:28 +0530

    properties

--- classes.py
+++ classes.py
@@ -12,8 +12,18 @@ class Dog(Animal):
     def Noise(self):
         print('Bark like an animal')
 
+    @property
+    def Name(self):
+        return self.__name
+        
+    @Name.setter
+    def Name(self, val):
+        self.__name = val
+
 
 if __name__ == "__main__":
     dog = Dog()
     dog.Noise()
     dog.Limbs()
+    dog.Name = 'Bonzo'
+    print(dog.Name)
